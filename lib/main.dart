2e4358bb83cda18a7d9dc54e247304357d9722e4
Aman Agarwal <amanagarwal2423@gmail.com>
2020-05-24 17:10:47 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:clima/screens/loading_screen.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
